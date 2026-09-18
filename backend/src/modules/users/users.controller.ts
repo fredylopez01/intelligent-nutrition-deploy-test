4bd439608cc7c35fe934b8a483f03dc40532fddf
fredylopez01 <lopezdazafredy@gmail.com>
2026-09-18 09:30:12 -0500

    feat(users): add endpoint to change user role AB#174

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,8 +1,20 @@
-import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto.js";
 import { UsersService } from "./users.service.js";
 import { Roles } from "../../common/decorators/roles.decorator.js";
 import { ListUsersQueryDto } from "./dto/list-users-query.dto.js";
+import { ChangeUserRoleDto } from "./dto/change-user-role.dto.js";
+import { CurrentUser } from "../../common/decorators/current-user.decorator.js";
+import type { AuthenticatedUser } from "../../common/interfaces/AuthenticatedUser.js";
 
 @Controller("users")
 export class UsersController {
@@ -18,5 +30,15 @@ export class UsersController {
   @Roles("SUPER ADMIN")
   findAll(@Query() query: ListUsersQueryDto) {
     return this.usersService.findAll(query);
+  }
+
+  @Patch("role/:id")
+  @Roles("SUPER ADMIN")
+  changeRole(
+    @Param("id", new ParseUUIDPipe()) id: string,
+    @Body() changeUserRoleDto: ChangeUserRoleDto,
+    @CurrentUser() currentUser: AuthenticatedUser,
+  ) {
+    return this.usersService.changeRole(id, changeUserRoleDto, currentUser);
   }
 }
